41365aea27608c38c913217c238d1cb65bbf975f
Xander Harris <plebis@hotmail.com>
2021-07-04 10:02:39 -0700

    Update backend
    See #1

--- main.tf
+++ main.tf
@@ -5,9 +5,11 @@ provider "google" {
 }
 
 terraform {
-    backend "gcs" {
-        bucket  = "xander-the-harris-jenkins-terraform-state"
-        prefix  = "terraform/state"
-    }
-}
+  backend "remote" {
+    organization = "brick-house"
 
+    workspaces {
+      name = "brick-house-github"
+    }
+  }
+}
