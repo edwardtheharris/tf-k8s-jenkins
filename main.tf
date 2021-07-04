2cd75edded04fde13a149fda43f7e82da69bc9bd
Xander Harris <plebis@hotmail.com>
2021-07-04 09:39:23 -0700

    Update terraform backend
    See #1

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ provider "google" {
     zone = "us-west-2a"
 }
 
+terraform {
+    backend "gcs" {
+        bucket  = "xander-the-harris-jenkins-terraform-state"
+        prefix  = "terraform/state"
+    }
+}
+
