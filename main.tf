1ed20bb0eff22bfb70e6d6069d2523a2c40ac79d
Xander Harris <plebis@hotmail.com>
2021-07-04 11:01:46 -0700

    Add var, set default
    See #1

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file(var.credentials_file)
+  credentials = var.credentials_file
   project     = "xander-the-harris-jenkins"
   region      = "us-west-2"
   zone        = "us-west-2a"
--- variables.tf
+++ variables.tf
@@ -1,1 +1,3 @@
-variable "credentials_file" {}
+variable "credentials_file" {
+    default = file("/usr/local/src/github.com/edwardtheharris/jenkins-terraform-gcp/xander-the-harris-jenkins-d72b6353f33d.json")
+}
