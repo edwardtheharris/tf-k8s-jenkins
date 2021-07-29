bf1f3661d755681ea984b03cc3ed6fddb822c3b3
Xander Harris <plebis@hotmail.com>
2021-07-28 17:34:58 -0700

    Drop duplicate backend
    See #3

--- main.tf
+++ main.tf
@@ -6,16 +6,6 @@ provider "google" {
 }
 
 terraform {
-  backend "remote" {
-    organization = "brick-house"
-
-    workspaces {
-      name = "brick-house-github"
-    }
-  }
-}
-
-terraform {
   backend "gcs" {
     bucket  = "xander-the-harris-jenkins-terraform-state"
     prefix  = "terraform/state"
