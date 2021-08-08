e2734b289c124e28372e75ee144566bd6b19e749
Xander Harris <plebis@hotmail.com>
2021-08-08 14:17:42 -0700

    Adjust outputs
    See #11

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,19 +1,19 @@
-output "region" {
-  value       = var.region
+output "gcp_region" {
+  value       = var.gcp_region
   description = "GCloud Region"
 }
 
-output "project_id" {
-  value       = var.project_id
+output "gcp_project_id" {
+  value       = var.gcp_project_id
   description = "GCloud Project ID"
 }
 
-output "kubernetes_cluster_name" {
-  value       = google_container_cluster.jenkins.name
+output "gke_cluster_name" {
+  value       = gcp.google_container_cluster.jenkins.name
   description = "GKE Cluster Name"
 }
 
-output "kubernetes_cluster_host" {
-  value       = google_container_cluster.jenkins.endpoint
+output "gke_cluster_host" {
+  value       = gcp.google_container_cluster.jenkins.endpoint
   description = "GKE Cluster Host"
 }
