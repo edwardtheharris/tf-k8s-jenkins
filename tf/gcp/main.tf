6bc5c6403de0cf46233ac9abfe2950bbbcc54c5e
Xander Harris <plebis@hotmail.com>
2021-08-08 14:06:32 -0700

    Add gcp module
    See #11

---
+++ tf/gcp/main.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  access_token = var.access_token
+  project      = var.project_id
+  region       = var.region
+  zone         = var.zone_id
+}
+
+# Retrieve an access token as the Terraform runner
+data "google_client_config" "provider" {}
+
+data "google_container_cluster" "jenkins" {
+  name     = google_container_cluster.jenkins.name
+  location = var.region
+}
+
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.jenkins.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.jenkins.master_auth[0].cluster_ca_certificate,
+  )
+}
