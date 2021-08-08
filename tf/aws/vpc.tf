cbf0b4eec581a08e7eb733622296bee6e64260ad
Xander Harris <plebis@hotmail.com>
2021-08-08 16:15:18 -0700

    Add subnets
    See #12

--- tf/aws/vpc.tf
+++ tf/aws/vpc.tf
@@ -7,33 +7,33 @@ resource "aws_vpc" "jenkins" {
 }
 
 resource "aws_subnet" "us-west-2a" {
-    availability_zone = "us-west-2a"
-    cidr_block = "10.0.0.0/24"
-    tags = {
-        Name = "us-west-2a"
-    }
+  availability_zone = "us-west-2a"
+  cidr_block        = "10.0.0.0/24"
+  tags = {
+    Name = "us-west-2a"
+  }
 }
 
 resource "aws_subnet" "us-west-2b" {
-    availability_zone = "us-west-2b"
-    cidr_block = "10.0.1.0/24"
-    tags = {
-        Name = "us-west-2b"
-    }
+  availability_zone = "us-west-2b"
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "us-west-2b"
+  }
 }
 
 resource "aws_subnet" "us-west-2c" {
-    availability_zone = "us-west-2c"
-    cidr_block = "10.0.2.0/24"
-    tags = {
-        Name = "us-west-2c"
-    }
+  availability_zone = "us-west-2c"
+  cidr_block        = "10.0.2.0/24"
+  tags = {
+    Name = "us-west-2c"
+  }
 }
 
 resource "aws_subnet" "us-west-2d" {
-    availability_zone = "us-west-2d"
-    cidr_block = "10.0.3.0/24"
-    tags = {
-        Name = "us-west-2d"
-    }
+  availability_zone = "us-west-2d"
+  cidr_block        = "10.0.3.0/24"
+  tags = {
+    Name = "us-west-2d"
+  }
 }
